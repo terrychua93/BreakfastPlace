57badd91e0ea7b12bfafbcc283e8681cc8723120
terrychua <terrychua93@gmail.com>
2025-01-14 22:24:45 +0800

    add: tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import { DatePickerModule } from 'primeng/datepicker';
+import { SelectModule } from 'primeng/select';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +16,10 @@ import { RouterOutlet } from '@angular/router';
     RouterOutlet,
     BrowserAnimationsModule,
     CommonModule,
+    DatePickerModule,
+    SelectModule,
+    InputTextModule,
+    TextareaModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
