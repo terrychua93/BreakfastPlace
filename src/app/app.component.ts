e043e46f1854b8c5eac84fee4b1cbc7bc3ccfffb
terrychua <terrychua93@gmail.com>
2025-01-14 17:04:59 +0800

    fix: integrate primeng framework ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppConfigService } from './app-config.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'breakfast_place';
+  theme: string;
+
+  constructor(private configService: AppConfigService) {
+    this.theme = this.configService.getConfig().theme;
+  }
 }
